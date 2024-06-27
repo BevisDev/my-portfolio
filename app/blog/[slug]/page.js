449fb64ab7a987c9c1fdc3ee9c56c6106c145fcf
BevisDev <dev.binhtt@gmail.com>
2024-06-27 16:53:57 +0700

    fixed blog slug

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,24 +2,45 @@
 import { personalData } from "@/utils/data/personal-data";
 
 async function getBlog(slug) {
-  const res = await fetch(`https://dev.to/api/articles/${personalData.devUsername}/${slug}`)
+  const res = await fetch(
+    `https://dev.to/api/articles/${personalData.devUsername}/${slug}`
+  );
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
 
   const data = await res.json();
   return data;
-};
+}
 
-async function BlogDetails({params}) {
+// Fetch all slugs to generate static paths
+async function fetchAllSlugs() {
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.devUsername}`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  return data.map((article) => article.slug);
+}
+
+// Generate static paths
+export async function generateStaticParams() {
+  const slugs = await fetchAllSlugs();
+
+  return slugs.map((slug) => ({
+    slug,
+  }));
+}
+
+async function BlogDetails({ params }) {
   const slug = params.slug;
   const blog = await getBlog(slug);
- 
-  return (
-    <div>
-    </div>
-  );
-};
+
+  return <div></div>;
+}
 
 export default BlogDetails;
--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   images: {
     domains: ["res.cloudinary.com", "media.dev.to"],
+    unoptimized: true,
   },
   output: "export",
 };
