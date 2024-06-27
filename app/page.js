00fad988718015ed82e5e96bda4fae4f29a93b4e
BevisDev <dev.binhtt@gmail.com>
2024-06-27 17:53:55 +0700

    open comment blog

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -5,23 +6,28 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import Blog from "./components/homepage/blog";
 
-// async function getData() {
-//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+async function getData() {
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.devUsername}`
+  );
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+  const filtered = data
+    .filter((item) => item?.cover_image)
+    .sort(() => Math.random() - 0.5);
 
-//   return filtered;
-// };
+  return filtered;
+}
 
 export default async function Home() {
-  // const blogs = await getData();
+  const blogs = await getData();
 
   return (
     <>
@@ -31,7 +37,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Blog blogs={blogs} />
       <ContactSection />
     </>
   );
