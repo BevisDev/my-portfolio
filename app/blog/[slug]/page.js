6c27bbdfc197acffb412ad0ea8d3cfd0f72a6c49
BevisDev <dev.binhtt@gmail.com>
2024-06-28 00:54:49 +0700

    updated blog slug

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -14,9 +14,22 @@ async function getBlog(slug) {
   return data;
 }
 
+// Fetch all slugs to generate static paths
+async function fetchAllSlugs() {
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.username}`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  return data.map((article) => article.slug);
+}
+
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
-  const slugs = await getBlog();
+  const slugs = await fetchAllSlugs();
 
   return slugs.map((slug) => ({
     slug,
