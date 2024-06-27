a3fc1a869c553874abc3fc913b40f99a610bc1d9
BevisDev <dev.binhtt@gmail.com>
2024-06-27 17:44:06 +0700

    comment blog

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,50 +1,50 @@
-import { personalData } from "@/utils/data/personal-data";
+// import { personalData } from "@/utils/data/personal-data";
 
-// Fetch blog data by slug
-async function getBlog(slug) {
-  const res = await fetch(
-    `https://dev.to/api/articles/${personalData.devUsername}/${slug}`
-  );
+// // Fetch blog data by slug
+// async function getBlog(slug) {
+//   const res = await fetch(
+//     `https://dev.to/api/articles/${personalData.devUsername}/${slug}`
+//   );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  const data = await res.json();
-  return data;
-}
+//   const data = await res.json();
+//   return data;
+// }
 
-// Fetch all slugs to generate static paths
-async function fetchAllSlugs() {
-  const res = await fetch(
-    `https://dev.to/api/articles?username=${personalData.devUsername}`
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+// // Fetch all slugs to generate static paths
+// async function fetchAllSlugs() {
+//   const res = await fetch(
+//     `https://dev.to/api/articles?username=${personalData.devUsername}`
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  const data = await res.json();
-  return data.map((article) => article.slug);
-}
+//   const data = await res.json();
+//   return data.map((article) => article.slug);
+// }
 
-// Generate static paths
-export async function generateStaticParams() {
-  const slugs = await fetchAllSlugs();
+// // Generate static paths
+// export async function generateStaticParams() {
+//   const slugs = await fetchAllSlugs();
 
-  return slugs.map((slug) => ({
-    slug,
-  }));
-}
+//   return slugs.map((slug) => ({
+//     slug,
+//   }));
+// }
 
-// Blog details component
-export default async function BlogDetails({ params }) {
-  const blog = await getBlog(params.slug);
+// // Blog details component
+// export default async function BlogDetails({ params }) {
+//   const blog = await getBlog(params.slug);
 
-  return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.description}</p>
-      <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>{blog.title}</h1>
+//       <p>{blog.description}</p>
+//       <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
+//     </div>
+//   );
+// }
