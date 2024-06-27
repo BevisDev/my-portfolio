336591a9ec334369e280a2729dcee310eb9f8e7b
BevisDev <dev.binhtt@gmail.com>
2024-06-28 01:11:56 +0700

    updated blog slug

--- app/page.js
+++ app/page.js
@@ -8,26 +8,24 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import Blog from "./components/homepage/blog";
 
-// async function getData() {
-//   const res = await fetch(
-//     `https://dev.to/api/articles?username=${personalData.username}`
-//   );
+async function getData() {
+  const res = await fetch("https://dev.to/api/articles?username=said7388");
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   const filtered = data
-//     .filter((item) => item?.cover_image)
-//     .sort(() => Math.random() - 0.5);
+  const filtered = data
+    .filter((item) => item?.cover_image)
+    .sort(() => Math.random() - 0.5);
 
-//   return filtered;
-// }
+  return filtered;
+}
 
 export default async function Home() {
-  // const blogs = await getData();
+  const blogs = await getData();
 
   return (
     <>
@@ -37,7 +35,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Blog blogs={blogs} />
       <ContactSection />
     </>
   );
