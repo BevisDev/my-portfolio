3e7274347a5861be716f14fb1d758fda0fd7cd75
BevisDev <dev.binhtt@gmail.com>
2024-06-28 00:59:55 +0700

    comment blog slug

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,52 +1,52 @@
-import { personalData } from "@/utils/data/personal-data";
+// import { personalData } from "@/utils/data/personal-data";
 
-// Fetch blog data by slug
-async function getBlog(slug) {
-  const res = await fetch(
-    `https://dev.to/api/articles/${personalData.username}/${slug}`
-  );
+// // Fetch blog data by slug
+// export async function getBlog(slug) {
+//   const res = await fetch(
+//     `https://dev.to/api/articles/${personalData.username}/${slug}`
+//   );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  const data = await res.json();
-  return data;
-}
+//   const data = await res.json();
+//   return data;
+// }
 
-// Fetch all slugs to generate static paths
-async function fetchAllSlugs() {
-  const res = await fetch(
-    `https://dev.to/api/articles?username=${personalData.username}`
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+// // Fetch all slugs to generate static paths
+// async function fetchAllSlugs() {
+//   const res = await fetch(
+//     `https://dev.to/api/articles?username=${personalData.username}`
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  const data = await res.json();
-  return data.map((article) => article.slug);
-}
+//   const data = await res.json();
+//   return data.map((article) => article.slug);
+// }
 
-// Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-  const slugs = await fetchAllSlugs();
+// // Return a list of `params` to populate the [slug] dynamic segment
+// export async function generateStaticParams() {
+//   const slugs = await fetchAllSlugs();
 
-  return slugs.map((slug) => ({
-    slug,
-  }));
-}
+//   return slugs.map((slug) => ({
+//     slug,
+//   }));
+// }
 
-// Blog details component
-// Multiple versions of this page will be statically generated
-// using the `params` returned by `generateStaticParams`
-export default async function BlogDetails({ params }) {
-  // const blog = await getBlog(params.slug);
+// // Blog details component
+// // Multiple versions of this page will be statically generated
+// // using the `params` returned by `generateStaticParams`
+// export default async function BlogDetails({ params }) {
+//   const blog = await getBlog(params.slug);
 
-  return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.description}</p>
-      <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>{blog.title}</h1>
+//       <p>{blog.description}</p>
+//       <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
+//     </div>
+//   );
+// }
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -55,14 +55,14 @@ function Navbar() {
             </Link>
           </li>
           <li>
-            <Link
+            {/* <Link
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="/blog"
             >
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
                 BLOGS
               </div>
-            </Link>
+            </Link> */}
           </li>
           <li>
             <Link
